cc4bd02ef9cbbd1156d869cd5ba1c0589b293a09
Benjamin Crews <aceF22@gmail.com>
2020-01-05 20:50:28 -0600

    Update gameStats.py
    Debug to take out custom tickmarks. Previous commit didn't fix it.

--- py/gameStats.py
+++ py/gameStats.py
@@ -175,8 +175,8 @@ def ecoIndexPlot(df, w=[1, 1.2, 0.7, 0.85, 1.35]):
     ax.legend()
     ax.set_title('Economic Power', fontsize=24)
     ax.text(0.005, 0.01, s='weights={}'.format(w), transform=ax.transAxes)
-    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
-    ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
+    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+    # ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
     
     return fig, ax
 
@@ -199,8 +199,8 @@ def teamTroopPlot(df, team='red'):
     ax.set_title('{} Team Troops\n'.format(team.title()), fontsize=24)
     ax.set_xlabel(None)
 
-    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
-    ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
+    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+    # ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
     
     return fig, ax
 
@@ -229,8 +229,8 @@ def troopIndexPlot(df, w=[1, 1, 1, 1, 0, 1, 1]):
     ax.legend()
     ax.set_title('Military Power', fontsize=24)
     ax.text(0.005, 0.01, s='weights={}'.format(w), transform=ax.transAxes)
-    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
-    ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute)
+    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+    # ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute)
     
     return fig, ax
 
