1388b7ac53234611bcc2acda712a0ecce27e6381
Benjamin Crews <aceF22@gmail.com>
2020-01-05 20:56:41 -0600

    Update gameStats.py
    Testing...

--- py/gameStats.py
+++ py/gameStats.py
@@ -136,8 +136,8 @@ def teamResourcePlot(df, team='red', p=True):
         ax1.set_title('{} Team Resources\n'.format(team.title()), fontsize=24)
         ax1.set_xlabel(None)
         
-        # ax1.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
-        # ax2.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+        ax1.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+        ax2.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
        
         # ax1.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
         # ax2.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
@@ -175,7 +175,7 @@ def ecoIndexPlot(df, w=[1, 1.2, 0.7, 0.85, 1.35]):
     ax.legend()
     ax.set_title('Economic Power', fontsize=24)
     ax.text(0.005, 0.01, s='weights={}'.format(w), transform=ax.transAxes)
-    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
     # ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
     
     return fig, ax
@@ -199,7 +199,7 @@ def teamTroopPlot(df, team='red'):
     ax.set_title('{} Team Troops\n'.format(team.title()), fontsize=24)
     ax.set_xlabel(None)
 
-    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
     # ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute
     
     return fig, ax
@@ -229,7 +229,7 @@ def troopIndexPlot(df, w=[1, 1, 1, 1, 0, 1, 1]):
     ax.legend()
     ax.set_title('Military Power', fontsize=24)
     ax.text(0.005, 0.01, s='weights={}'.format(w), transform=ax.transAxes)
-    # ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))      # Major tick every 10 mins
     # ax.xaxis.set_minor_locator(mdates.MinuteLocator(interval=1))       # Minor tick every minute)
     
     return fig, ax
